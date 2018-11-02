ec23304e16554124f0f83e9ec6ae8c0ea66ba76c
Steve Hobbs <elkdanger@gmail.com>
2018-11-02 12:41:39 +0000

    Created very basic API server

--- 01-login/vue.config.js
+++ 01-login/vue.config.js
@@ -2,7 +2,8 @@ const webpack = require("webpack");
 
 module.exports = {
   devServer: {
-    port: 3000
+    port: 3000,
+    proxy: "http://localhost:3001"
   },
   configureWebpack: {
     plugins: [
