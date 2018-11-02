9483960ade30c21740a47b3ada844f9adeb40665
Steve Hobbs <elkdanger@gmail.com>
2018-11-02 11:25:50 +0000

    Moved app URI to a configuration setting

--- 01-login/src/authService.js
+++ 01-login/src/authService.js
@@ -3,7 +3,7 @@ import { EventEmitter } from "events";
 
 const webAuth = new auth0.WebAuth({
   domain: process.env.VUE_APP_AUTH0_DOMAIN,
-  redirectUri: "http://localhost:3000/callback",
+  redirectUri: `${process.env.VUE_APP_URI}/callback`,
   clientID: process.env.VUE_APP_AUTH0_CLIENTID,
   responseType: "id_token",
   scope: "openid profile email"
