a36a058512e11c6a8b9c07347b56244af1761ab3
Steve Hobbs <elkdanger@gmail.com>
2018-11-02 13:59:25 +0000

    Added some API endpoints

--- 01-login/server.js
+++ 01-login/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
+app.get("/api/private", (req, res) => {
+  res.send({
+    msg: "Your ID token was successfully validated!"
+  });
+});
+
+app.get("/api/admin", (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
 
 module.exports = app;
