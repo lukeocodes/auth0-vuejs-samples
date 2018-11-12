53882b349c0b73dc9da092d07df875e1b6ef23f5
Steve Hobbs <elkdanger@gmail.com>
2018-11-12 12:56:15 +0000

    Added authentication to private API call

--- 01-login/server.js
+++ 01-login/server.js
@@ -2,21 +2,34 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 const app = express();
+
+if (!process.env.VUE_APP_AUTH0_DOMAIN || !process.env.VUE_APP_AUTH0_CLIENTID) {
+  throw "Make sure you have VUE_APP_AUTH0_DOMAIN and VUE_APP_AUTH0_CLIENTID in your .env file";
+}
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 
-app.get("/api/private", (req, res) => {
-  res.send({
-    msg: "Your ID token was successfully validated!"
-  });
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.VUE_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  audience: process.env.VUE_APP_AUTH0_CLIENTID,
+  issuer: `https://${process.env.VUE_APP_AUTH0_DOMAIN}/`,
+  algorithm: ["RS256"]
 });
 
-app.get("/api/admin", (req, res) => {
+app.get("/api/private", checkJwt, (req, res) => {
   res.send({
-    msg: "Your access token was successfully validated!"
+    msg: "Your ID token was successfully validated!"
   });
 });
 
